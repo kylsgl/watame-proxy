3a157bd9acdf8edc3df986cf0d132eddde25a382
kylsgl <kylsgl4@Gmail.com>
2023-05-01 16:48:18 +0800

    refactor(server): handle error from the root

--- src/server.ts
+++ src/server.ts
@@ -4,29 +4,23 @@ import fastify from 'fastify';
 
 /* eslint-disable no-console */
 async function build(): Promise<void> {
-	try {
-		const port = Number(process.env.PORT ?? 8080);
-		const host: string = process.env.HOST ?? '::';
+	const port = Number(process.env.PORT ?? 8080);
+	const host: string = process.env.HOST ?? '::';
 
-		const server = fastify({
-			logger: false,
-			connectionTimeout: 15e3,
-			keepAliveTimeout: 15e3,
-			pluginTimeout: 3e4,
-			requestTimeout: 15e3,
-			trustProxy: true,
-		});
+	const server = fastify({
+		logger: false,
+		connectionTimeout: 15e3,
+		keepAliveTimeout: 15e3,
+		pluginTimeout: 3e4,
+		requestTimeout: 15e3,
+		trustProxy: true,
+	});
 
-		await server.register(import('./app'));
-
-		await server.listen({ port, host });
-	} catch (error) {
-		console.error(error);
-		process.exit(1);
-	}
+	await server.register(import('./app'));
+	await server.listen({ port, host });
 }
 
-async function init(): Promise<void> {
+async function start(): Promise<void> {
 	const instance = Number(process.env.INSTANCE ?? 1);
 	if (instance !== 1 && instance >= 0 && cluster.isPrimary) {
 		const numInstance: number = instance === 0 ? cpus().length : instance;
@@ -52,4 +46,7 @@ async function init(): Promise<void> {
 	console.info(`Worker ${process.pid} started`);
 }
 
-await init();
+start().catch((error: unknown): void => {
+	console.error(error);
+	process.exit(1);
+});
